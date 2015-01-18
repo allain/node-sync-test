26f3b29dbd73ee97aff9ca57a97a40d5c5c5d79d
allain <allain.lalonde@gmail.com>
2015-01-18 00:52:16 -0500

    using dom-delegate-stream

--- public/client.js
+++ public/client.js
@@ -1,30 +1,37 @@
 // file: client.js
 var debug = require('debug')('sync-client');
+var fs = require('fs')
 
 var SynopsisClient= require('synopsis-client');
 
 var Handlebars = require('handlebars-stream');
 
-var fs = require('fs');
-var $ = require('jquery');
+var DomDelegate = require('dom-delegate-stream');
 
 var store = window.test = new SynopsisClient('test');
+var writable = require('writable');
+
+var $ = require('jquery');
 
 var HtmlPatcherStream = require('html-patcher-stream');
 
 store.pipe(Handlebars(fs.readFileSync(__dirname + '/ui.hbs', 'utf8')))
   .pipe(HtmlPatcherStream(document.getElementById('app'), '<div></div>'));
 
-$("#app").on('keyup', '#new-todo', function(e) {
-  if (e.keyCode !== 13) return;
+var app = DomDelegate(document.getElementById('app'));
+app.on('keyup', '#new-todo').pipe(writable({objectMode: true}, function(e, encoding, cb) {
+  if (e.keyCode != 13) return;
+
   e.preventDefault();
 
   addTodo();
-});
+  cb();
+}));
 
-$("#app").on('click', '#add-todo', function(e) {
+app.on('click', '#add-todo').pipe(writable({objectMode: true}, function(e, encoding, cb) {
   addTodo();
-});
+  cb();
+}));
 
 function addTodo() {
   var title = $('#new-todo').val().trim();
@@ -36,9 +43,12 @@ function addTodo() {
   }
 }
 
-$("#app").on('click', '.completed', function(e) {
-  var id = $(this).parent().attr('data-id');
+app.on('click', '.completed').pipe(writable({objectMode: true}, function(e) {
+  var id = e.target.dataset.id;
+
   store.edit(function(state) {
     state.todos = state.todos.filter(function(t) { return t.id != id; });
   });
-});
+
+  $('.completed').removeAttr('checked');
+}));
