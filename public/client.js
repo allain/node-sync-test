0fa0af4c0786632fdd7866c01f3a1c8f94a120c7
allain <allain.lalonde@gmail.com>
2015-01-20 16:55:10 -0500

    authing better

--- public/client.js
+++ public/client.js
@@ -2,7 +2,7 @@
 var debug = require('debug')('sync-client');
 
 var fs = require('fs')
-
+var through = require('through2').obj;
 var SynopsisClient= require('synopsis-client');
 
 var Handlebars = require('handlebars-stream');
@@ -11,28 +11,58 @@ var DomDelegate = require('dom-delegate-stream');
 
 var store = window.test = new SynopsisClient('test');
 var writable = require('writable');
-var $ = require('jquery');
 
+var $ = require('jquery');
 var HtmlPatcherStream = require('html-patcher-stream');
 
+var mergeStream = require('object-merge-stream');
+
 var hello = window.hello = require('hellojs');
-hello.on('auth auth.login', function(auth){
+
+var appState = mergeStream({});
+
+$("body").delegate('#google-login', 'click', function(e) {
+	hello('google').login();
+});
+
+$("body").delegate('#logout', 'click', function(e) {
+	hello.logout();
+});
+
+appState.push({authed: false});
+
+hello.on('auth.login', function(auth){
   // call user information, for the given network
 	hello(auth.network).api( '/me' ).then( function(r) {
-  	document.getElementById('profile').innerHTML = '<img src="'+ r.thumbnail +'" /> Hey '+r.name + '<a href="#" id="logout">Logout</a>';
-
-    $("#logout").click(function(e) {
-      hello.logout();
-  	});    
+    appState.push({
+		  authed: true,	
+			profile: r
+		});
 	});
+});
+
+hello.on('auth.logout', function(){
+  appState.push({
+		profile: null,
+    authed: true
+  });
 });
 
 hello.init({ 
 	google: '100706142658-6iaoqf1pak20cso1shbq7slsmrcaeis6.apps.googleusercontent.com'
 }, {redirect_uri:'/redirect.html'});
+
+
  
-store.pipe(Handlebars(fs.readFileSync(__dirname + '/ui.hbs', 'utf8')))
-  .pipe(HtmlPatcherStream(document.getElementById('app')));
+store
+.pipe(appState)
+.pipe(through(function(chunk, enc, cb) {
+  console.log(chunk); 
+  this.push(chunk);
+	cb();
+}))
+.pipe(Handlebars(fs.readFileSync(__dirname + '/ui.hbs', 'utf8')))
+.pipe(HtmlPatcherStream(document.getElementById('app')));
 
 var app = DomDelegate(document.getElementById('app'));
 app.on('keyup', '#new-todo').pipe(writable({objectMode: true}, function(e) {
